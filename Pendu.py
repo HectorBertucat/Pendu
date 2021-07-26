3343d0ee5ff18a4e47d19d840979f8ae7ec8b1e3
Hector <hectorbertucat@gmail.com>
2021-07-26 13:12:21 +0200

    initialization of the game

---
+++ Pendu.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+print("Bienvenue sur le jeu le Pendu par Hector Bertucat\n")
+name=input("Quel est votre nom : ")
+print("Bonjour " + name + ", bonne chance !")
+time.sleep(3)
+print("Le jeu va commencer...\n C'est parti pour le Pendu !")
+time.sleep(3)
